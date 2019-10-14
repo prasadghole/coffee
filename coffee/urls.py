3abbf1a6a5968c6dd34e0f8ab938a1bd15ebf94c
Prasad Ghole <prasad.ghole@gmail.com>
2019-10-14 16:15:52 +0530

    app about added

--- coffee/urls.py
+++ coffee/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from coffee.about import views as about_views
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',TemplateView.as_view(template_name='homepage.html')),
+    path('about/',about_views.contact),
 ]
